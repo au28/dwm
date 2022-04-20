b7007584e874d547914ccb248ac896c53e42b40e
Adeodatus <adtsaug@gmail.com>
2022-04-20 21:09:10 +0530

    Customize fonts

--- config.h
+++ config.h
@@ -12,7 +12,10 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 25;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=14" };
+static const char *fonts[]          = {
+	"JetBrains Mono Bold:size=14:antialias=true:autohint=true",
+	"JoyPixels:size=14:antialias=true:autohint=true"
+};
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
